07ec3c01c6195ef1c679856e18337438b44260c4
Gustavo Santos <root.gusta@gmail.com>
2026-01-28 15:47:54 -0300

    feat: add controller. file search_select.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,23 +1,4 @@
-from src.filesystem.scanner import scan_for_pst_files_full
-from src.interface.cli import select_pst_files
-
-
-def main():
-    print("Iniciando varredura completa de arquivos PST...")
-    pst_files = scan_for_pst_files_full()
-
-    if not pst_files:
-        print("Nenhum arquivo PST encontrado.")
-        return
-
-    print(f"\nEncontrados {len(pst_files)} arquivos PST:\n")
-
-    selected_files = select_pst_files(pst_files)
-
-    print("\nArquivos selecionados:")
-    for file in selected_files:
-        print(file)
-
+from src.controller.search_select import run_search_select
 
 if __name__ == "__main__":
-    main()
+    run_search_select()
---
+++ src/controller/search_select.py
@@ -0,0 +1,18 @@
+from src.filesystem.scanner import scan_for_pst_files_full
+from src.interface.cli import select_pst_files
+
+def run_search_select():
+    print("Iniciando varredura completa de arquivos PST...")
+    pst_files = scan_for_pst_files_full()
+
+    if not pst_files:
+        print("Nenhum arquivo PST encontrado.")
+        return
+
+    print(f"\nEncontrados {len(pst_files)} arquivos PST:\n")
+
+    selected_files = select_pst_files(pst_files)
+
+    print("\nArquivos selecionados:")
+    for file in selected_files:
+        print(file)
